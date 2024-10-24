bef04361e87351e864e70ac65fb619bfffdc806a
nazguul512 <george.cindea@gmail.com>
2024-10-24 12:51:41 +0300

    add workflows and modify links

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='tradingview_ta',
-    version='3.3.0',
+    name='tradingview_ta_v3',
+    version='1.0.0',
     description="Unofficial TradingView technical analysis API wrapper.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/brian-the-dev/python-tradingview-ta',
-    author='Brian',
-    author_email='brian@brianthe.dev',
-    packages=['tradingview_ta'],
+    url='https://github.com/nazguul512/python-tradingview-ta-v3',
+    author='nazguul512',
+    author_email='',
+    packages=['tradingview_ta_v3'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -20,11 +20,12 @@ setuptools.setup(
     ],
     install_requires=[
           'requests',
+          'colorama'
     ],
     python_requires='>=3.6',
     project_urls={
         'Demo': 'https://tradingview.brianthe.dev',
         'Documentation': 'https://python-tradingview-ta.readthedocs.io',
-        'Source': 'https://github.com/brian-the-dev/python-tradingview-ta',
+        'Source': 'https://github.com/nazguul512/python-tradingview-ta-v3',
     },
 )
